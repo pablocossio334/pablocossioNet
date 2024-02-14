045bec2df4546ec7b2bf95f24c02d4699d8cfb1e
Pablo Cossio <pablocossio@gmail.com>
2024-02-14 17:56:20 -0300

    pruebaMovil22

--- src/components/MainD.jsx
+++ src/components/MainD.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Crea from '../assets/CREA.jpg'
 import Apuntes from '../assets/apuntes.png'
 import CuboAmarillo from '../assets/Cubo-Amarillo.png'
+import MarioAnimado from '../assets/mario.gif'//src\assets\mario.gif
 
 const MainD = () => {
   return (
@@ -12,6 +13,10 @@ const MainD = () => {
       <div className="linkD"><img src={CuboAmarillo} alt="" /></div>
       <div className="linkD"><img src={CuboAmarillo} alt="" /></div>
       
+    </div>
+    <div className="animaD">
+caca
+      <img src={<MarioAnimado/>} alt="" />
     </div>
     </div>
   )
